b94fa736d58817ff6509da861f1eaf7d59e2f873
Sulaiman-Alam <108904299+Sulaiman-Alam@users.noreply.github.com>
2025-01-20 10:43:54 -0500

    Add files via upload

---
+++ Home/home.js
@@ -0,0 +1,8 @@
+// Audio code still in work
+
+// let myAudio = document.querySelector('#audio')
+// let button = document.querySelector("#sound");
+
+// button.addEventListener("click", () => {
+//     myAudio.play();
+// });
